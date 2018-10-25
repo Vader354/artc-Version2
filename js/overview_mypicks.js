89be264c97a41faf3281cd71f0479b79d448670a
Marie Henkelmann <MarieHenkelmann@Maries-Air-2.lan>
2018-10-25 14:43:08 +0200

    latest changes, tabs for overview, stylings, etc

---
+++ js/overview_mypicks.js
@@ -0,0 +1,26 @@
+// WIP!
+
+let venues = 
+{ 
+    museums = { ... },
+
+    galleries = { ... }
+}
+
+let exhibitions = 
+{
+    exhibition1 = { ... },
+    exhibition2 = { ... }
+
+}
+
+function addPicks(exhibitions) {
+    let myPicks = [];
+    // when clicking this and this button, push to the myPicks array;
+    myPicks = myPicks.exhibitions.push();
+    return myPicks;
+}
+
+function removePicks(exhibitions) {
+    
+}
