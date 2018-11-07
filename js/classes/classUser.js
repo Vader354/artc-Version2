5c254aa8e592104c508d12433355e3bd6878cfb8
Marie Henkelmann <MarieHenkelmann@Maries-Air-2.lan>
2018-11-07 16:33:19 +0100

    added User class

---
+++ js/classes/classUser.js
@@ -0,0 +1,21 @@
+class User {
+
+    // The constructor for our class, which will allow us to create new objects of our class
+    constructor (firstname, lastname, gender, dateOfBirth, password) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.gender = gender;
+      this.dateOfBirth = dateOfBirth;
+      this.password = this.encryptPwd(password);
+      this.lastAccess = null;
+    }
+
+    encryptPwd() {
+
+    }
+
+    setLastAccess() {
+        this.lastAccess = Date.now();  
+    }
+
+}
