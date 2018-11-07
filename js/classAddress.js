c7fddd60de29b1a7d0482f3c6d97848ad58a1a2d
Marie Henkelmann <MarieHenkelmann@Maries-MacBook-Air-2.local>
2018-11-07 10:59:40 +0100

    added class address file

---
+++ js/classAddress.js
@@ -0,0 +1,9 @@
+class Address {
+    constructor(street, streetNr, postalCode, city, country) {
+        this.street = street;
+        this.streetNr = streetNr;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.country = country;
+    }
+}
