f3a45b62ef51708c202778596b20928327b30063
Marie Henkelmann <MarieHenkelmann@Maries-MacBook-Air-2.local>
2018-11-07 11:23:22 +0100

    classAddress.js

--- js/classAddress.js
+++
@@ -1,9 +0,0 @@
-class Address {
-    constructor(street, streetNr, postalCode, city, country) {
-        this.street = street;
-        this.streetNr = streetNr;
-        this.postalCode = postalCode;
-        this.city = city;
-        this.country = country;
-    }
-}
