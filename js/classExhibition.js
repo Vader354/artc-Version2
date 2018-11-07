65023c054baf4df15bcc32199555ffae9885797c
Marie Henkelmann <MarieHenkelmann@Maries-MacBook-Air-2.local>
2018-11-07 11:13:25 +0100

    last changes

--- js/classExhibition.js
+++ js/classExhibition.js
@@ -1,7 +1,8 @@
 class Exhibition {
-    constructor(exhibitionID, name, artType, artStyle, description, startDate, endDate) {
+    constructor(exhibitionID, name, venueID, artType, artStyle, description, startDate, endDate) {
         this.exhibitionID = exhibitionID;
         this.name = name;
+        this.venueID = venueID; 
         this.artType = artType;
         this.artStyle = artStyle;
         this.description = description;
