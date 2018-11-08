dffb0fbad944394586208f432a4752dcc31aea40
Marie Henkelmann <MarieHenkelmann@Maries-Air-2.lan>
2018-11-08 18:05:40 +0100

    worked on addPick function - WIP

--- js/classes/classExhibition.js
+++ js/classes/classExhibition.js
@@ -9,7 +9,7 @@ class Exhibition {
         this.endDate = endDate;
     }
 
-    // WIP
+    // WIP ...
     getAddress() {
         this.address = document.getElementsByClassName(Address)
     }
@@ -24,6 +24,33 @@ exhibitions.push(new Exhibition(123, "Luca Tombolini Exhibition", "photography",
 exhibitions.push(new Exhibition(456, "Frida Kahlo Exhibition", "painting", "colonial", "A painting series from Mexico", "2018-02-01", "2018-06-01"))
 exhibitions.push(new Exhibition(789, "Lee Woodman Exhibition", "sculptures", "contemporary", "A creative director working 3D", "2018-05-05", "2018-07-07"))
 
-
 console.log(exhibitions)
+
+var exhibition1 = exhibitions[0];
+var exhibition2 = exhibitions[1];
+var exhibition3 = exhibitions[2];
+
+
+// MY PICKS - WIP.
+
+var myPicks = [];
+
+addPick() {
+    let i = 0;
+    for (i = 0; i < exhibitions.length; i++) {
+        if (myPicks.includes.exhibitions[i]) {
+                false
+            } else {
+                myPicks.push.exhibitions[i];
+                alert(JSON.stingify(exhibitions[i]) + "has been added to your picks.");
+        }
+    }
+}
+
+
+removePick() {
+    // myPicks.slice[i] 
+}
+
+
 
