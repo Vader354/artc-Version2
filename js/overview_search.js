9a548fb60647a4e8e6e6ea711e0066e52ef1194b
EM <meyer.ehler@gmail.com>
2018-11-13 10:23:15 +0100

    Comments on what to do

--- js/overview_search.js
+++ js/overview_search.js
@@ -7,7 +7,7 @@ function searchFunction() {
     li = ul.getElementsByTagName('li');
 
     // Loop through all list items, and hide those who don't match the search query
-    // Go through code again to fully understand it
+    // TODO: Go through code again to fully understand it, implement it for venue tab, Filter nach Venue Type, Filter nach Artstyle etc.
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
@@ -17,3 +17,5 @@ function searchFunction() {
         }
     }
 }
+
+// GetElementsbyClass probieren, class für Venue UL und Exhibition UL bzw. lis implementieren, return a list
