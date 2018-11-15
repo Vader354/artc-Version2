9eae22217cca0365536da31edc9a303a60024c95
Marie Henkelmann <MarieHenkelmann@Maries-MacBook-Air-2.local>
2018-11-15 09:37:05 +0100

    my pick list

--- js/mypicklist.js
+++ js/mypicklist.js
@@ -2,11 +2,15 @@
 var currentPicks = localStorage.getItem("Picks");
 currentPicks = JSON.parse(currentPicks);
 
-
 function createTable(tableData) {
     var table = document.createElement('table');
     var tableBody = document.createElement('tbody');
   
+    for (var i; i < tableData.length; i++) {
+        var row = document.createElement("tr");
+        row.appendChild(document.create)
+    }
+    
     tableData.forEach(function(rowData) {
         var row = document.createElement('tr');
         row.appendChild(document.createTextNode(rowData));
@@ -16,6 +20,5 @@ function createTable(tableData) {
     table.appendChild(tableBody);
     document.body.appendChild(table);
 };
-
-
-document.getElementById("displayMyPicks").innerHTML = createTable(currentPicks); 
+  
+document.getElementById("displayMyPicks").innerHTML = createTable(currentPicks);
