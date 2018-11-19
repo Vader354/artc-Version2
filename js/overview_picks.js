ec311f9c3a9af4a6dd21a024525f5040dc0fc053
Marie Henkelmann <MarieHenkelmann@Maries-MacBook-Air-2.local>
2018-11-19 09:46:23 +0100

    updated mypicks and create HTML method

--- js/overview_picks.js
+++ js/overview_picks.js
@@ -1,42 +1,3 @@
-// MY PICKS - WIP.
-
-// var userPicks = [];
-
-/*
-// simple version with local storage, works, but doesn't check if object already is in the myPicks list
-function addPick(exhibition) {
-    userPicks.push(exhibition);
-    localStorage.setItem("Picks", JSON.stringify(userPicks));
-    alert(JSON.stringify(exhibition.name) + " has been added to your picks!")
-    console.log(userPicks);
-}
-*/
-/*
-// first check if item is already in local storage, if yes, it shouldnt be pushed
-function addPick(exhibition) {
-    var picksFromLS = localStorage.getItem("Picks");
-    console.log(picksFromLS);
-    picksFromLS = JSON.parse(picksFromLS);
-    console.log(picksFromLS);
-    // check if picks in local storage is empty, if yes push
-    if (picksFromLS == null) {
-        userPicks.push(exhibition);
-        localStorage.setItem("Picks", JSON.stringify(userPicks));
-        alert(JSON.stringify(exhibition.name) + " has been added to your picks."); 
-    } else {
-        console.log(picksFromLS);
-        if (picksFromLS.includes(exhibition)) {
-            return false;
-        } else {
-            userPicks.push(exhibition);
-            localStorage.setItem("Picks", JSON.stringify(userPicks));
-            alert(JSON.stringify(exhibition.name) + " has been added to your picks.");
-        }  
-    }
-};
-*/
-
-// eventListener ist wohl besser als onclick, aber funktioniert irgendwie nicht. onclick funktioniert mit myPicks
 
 // try to connect button array to exhibitions array
 
@@ -71,6 +32,7 @@ for (var i = 0; i < addPickButtons.length; i++) {
     addPickButtons[i].addEventListener("click", addPick);
 }
 
+
 // HOVER BEFORE REMOVE
 
 
@@ -95,3 +57,50 @@ for (var i = 0; i < removePickButtons.length; i++) {
 
 // => dass sich der button dann umwandelt in schwarz/aktiv mit Häckchen "Saved to my Picks"
 // und wenn man hovert man sieht "Remove from my Picks" ... addEventListener("hover", ... function style display bla)
+
+
+/*
+
+// NEW TRY
+
+
+var addPickButtons = document.getElementsByClassName("addPickButton");
+console.log(addPickButtons);
+console.log(exhibitions);
+
+// check which button was clicked, and execute addPick function
+for (var i = 0; i < addPickButtons.length; i++) {
+    addPickButtons[i].addEventListener("click", addPick);
+} 
+
+
+function addPick() {
+    var picksFromLS = localStorage.getItem("Picks");
+    var userPicks = JSON.parse(picksFromLS);
+    // check which exhibition is picked
+    var pickedExhibition = null;
+    for (var i = 0; i < addPickButtons.length; i++) {
+        if (addPickButtons.findIndex(addPickButtons[i]) == exhibitions.findIndex(exhibitions[i])) {
+            console.log(indexOf.addPickButtons[i]);
+            pickedExhibition = exhibitions[i];
+            console.log(pickedExhibition);
+            // return pickedExhibition;
+        } 
+    }
+    // check if picks in local storage is empty, if yes push
+    if (userPicks == null) {
+        userPicks = [];
+        userPicks.push(pickedExhibition);
+        localStorage.setItem("Picks", JSON.stringify(userPicks));
+        alert(JSON.stringify(pickedExhibition) + " has been added to your picks.");
+    } else {
+        if (userPicks.includes(pickedExhibition)) {
+            return false;
+        } else {
+            userPicks.push(pickedExhibition);
+            localStorage.setItem("Picks", JSON.stringify(userPicks));
+            alert(JSON.stringify(pickedExhibition) + " has been added to your picks.");
+        }  
+    }
+}; */
+
