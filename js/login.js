484f09319f4673bf71178b5088414c340ce6fc93
Gustaf Lindström <cali18ad@student.cbs.dk>
2018-11-21 10:33:41 +0100

    tried to create users in the submit function

--- js/login.js
+++ js/login.js
@@ -4,8 +4,7 @@
 var loginForm = document.getElementById("loginForm");
 var submit = document.getElementById("submit")
 // Retrieving stored users from localStorage
-var users = JSON.parse(localStorage.getItem("Users"));
-console.log(users)
+// var users =  JSON.parse(localStorage.getItem("Users"));
 // Creates a variable for maxium user login attempts
 var loginAttempts = 5;
 
@@ -18,6 +17,7 @@ var currentUser = [];
 
   // Eventlistener for clicking login. 
   submit.addEventListener("click", function(){
+    var users = (localStorage.getItem("Users") !== null) ? JSON.parse(localStorage.getItem("Users")) : alert("There is no user with that username");
     console.log("Eventlistener fired on click");
     // Variable for user input email
     var username = loginForm.email.value;
