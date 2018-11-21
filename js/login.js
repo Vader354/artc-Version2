736efce954dc07f586bd4b841463bf9a4b945930
Gustaf Lindström <cali18ad@student.cbs.dk>
2018-11-21 10:36:48 +0100

    ignore latest push, it was weird

--- js/login.js
+++ js/login.js
@@ -4,7 +4,8 @@
 var loginForm = document.getElementById("loginForm");
 var submit = document.getElementById("submit")
 // Retrieving stored users from localStorage
-// var users =  JSON.parse(localStorage.getItem("Users"));
+var users =  JSON.parse(localStorage.getItem("Users"));
+// var users = (localStorage.getItem("Users") !== null) ? JSON.parse(localStorage.getItem("Users")) : alert("There is no user with that username");
 // Creates a variable for maxium user login attempts
 var loginAttempts = 5;
 
@@ -17,7 +18,7 @@ var currentUser = [];
 
   // Eventlistener for clicking login. 
   submit.addEventListener("click", function(){
-    var users = (localStorage.getItem("Users") !== null) ? JSON.parse(localStorage.getItem("Users")) : alert("There is no user with that username");
+
     console.log("Eventlistener fired on click");
     // Variable for user input email
     var username = loginForm.email.value;
