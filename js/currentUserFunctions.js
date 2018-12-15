97a8aa0259caae98a635004e694912948c2351b2
Marie Henkelmann <MarieHenkelmann@maries-air-2.lan>
2018-12-15 21:32:41 +0100

    fixed mypicks list

--- js/currentUserFunctions.js
+++ js/currentUserFunctions.js
@@ -4,15 +4,15 @@
 window.addEventListener("load", function() {
     // checks if the localStorage contains a currentUser/loggedIn user
 	if (localStorage.getItem("CurrentUser") == null) {
-    // if localStorage is empty myPickList will be hidden from the user
-    document.getElementById("myPickList").style.display = "none";
-    // if localStorage is empty the buttons under overview will be hidden from the user
-    var hiddenButtons = document.getElementsByClassName("hiddenElement")
-    // loop to hide all buttons from user
-    for (var i = 0; i < hiddenButtons.length; i++) {
-        // hides the buttons from the user
-        hiddenButtons[i].style.display = "none";
-    }
+        // if localStorage is empty myPickList will be hidden from the user
+        document.getElementById("myPickList").style.display = "none";
+        // if localStorage is empty the buttons under overview will be hidden from the user
+        var hiddenButtons = document.getElementsByClassName("hiddenElement")
+        // loop to hide all buttons from user
+        for (var i = 0; i < hiddenButtons.length; i++) {
+            // hides the buttons from the user
+            hiddenButtons[i].style.display = "none";
+        }
     } // changes log in to log out 
     else if (localStorage.getItem("CurrentUser") !== null) {
         // if there is a currentUser in localStorage, the text of the log in button will be changed to log out
@@ -20,8 +20,8 @@ window.addEventListener("load", function() {
         // log out, change the functionality of the log-in button
         // Adds and eventListener to the login button, which will respond to a click
         document.getElementById("logInNav").addEventListener("click", function() {
-        // if there is a user in local storage, and the log in/log out button is clicked, the currentUser will be removed from localStorage
-        localStorage.removeItem("CurrentUser");
+            // if there is a user in local storage, and the log in/log out button is clicked, the currentUser will be removed from localStorage
+            localStorage.removeItem("CurrentUser");
         }) 
     }	
 })
--- js/mypicklist.js
+++ js/mypicklist.js
@@ -1,14 +1,37 @@
 // get current picks from local storage
-var currentPicks = localStorage.getItem("Picks");
-currentPicks = JSON.parse(currentPicks);
-  
+var currentPicks = JSON.parse(localStorage.getItem("Picks"));
+
+
+function createHTMLlist(exhibitions) {
+    return "<h2 style='font-size:22px'>" + exhibitions.name + "</h2>"
+    +   "<div class='venuename'>" + exhibitions.getVenueName() + "</div>"
+    +   "<div>" + exhibitions.startDate + " – " + exhibitions.endDate + "</div>"
+    +   "<div class='artType'>" + exhibitions.type + " " + exhibitions.artStyle + "</div>";
+}
+
+var content = "";
+
+    for (var i = 0; i < currentPicks.length; i++) {
+        for (var j = 0; j < exhibitions.length; j++) 
+            if (currentPicks[i] == exhibitions[j].name) {
+                console.log(exhibitions[j]);
+                content += createHTMLlist(exhibitions[j])
+            }
+        }
+
+document.getElementById("picklist").innerHTML = content;
+
+
+
+
+
+
+/*
 // cells are not really necessary for us now but maybe in the future when we work with real objects and want to display more than the name
 function createTable(tableArr) {
     // create a table object
     let table = document.createElement('table');
     // iterate over every array(row) within tableArr
-    // the for...of loop is the same (but shorter) as:
-    // for (let row = 0; row <= tableArr.length; row++)
     for (let row of tableArr) {
         // insert a new row element into the table element
         table.insertRow();
@@ -27,3 +50,4 @@ function createTable(tableArr) {
 
 // execute function to create table
 createTable(currentPicks);
+*/
