7671813983e39ab47e006bf96f8e7476a4d1723b
Marie Henkelmann <MarieHenkelmann@maries-air-2.lan>
2018-12-15 21:45:35 +0100

    commented new changes

--- js/mypicklist.js
+++ js/mypicklist.js
@@ -1,7 +1,7 @@
 // get current picks from local storage
 var currentPicks = JSON.parse(localStorage.getItem("Picks"));
 
-
+// creates HTML elemenet for the picklist
 function createHTMLlist(exhibitions) {
     return "<h2 style='font-size:22px'>" + exhibitions.name + "</h2>"
     +   "<div class='venuename'>" + exhibitions.getVenueName() + "</div>"
@@ -9,23 +9,25 @@ function createHTMLlist(exhibitions) {
     +   "<div class='artType'>" + exhibitions.type + " " + exhibitions.artStyle + "</div>";
 }
 
+// content variable set to empty
 var content = "";
 
-    for (var i = 0; i < currentPicks.length; i++) {
-        for (var j = 0; j < exhibitions.length; j++) 
-            if (currentPicks[i] == exhibitions[j].name) {
-                console.log(exhibitions[j]);
-                content += createHTMLlist(exhibitions[j])
-            }
+// nested loop: for each currentPicks element it goes through the exhibitions 
+// when the matching exhibition is found, the program puts calls the createHTMLlist function on the matched exhibition object and binds it to the contet variable
+for (var i = 0; i < currentPicks.length; i++) {
+    for (var j = 0; j < exhibitions.length; j++) 
+        if (currentPicks[i] == exhibitions[j].name) {
+            content += createHTMLlist(exhibitions[j])
         }
+}
 
+// content will be displayed in the picklist div
 document.getElementById("picklist").innerHTML = content;
 
 
 
 
-
-
+// OLD VERSION
 /*
 // cells are not really necessary for us now but maybe in the future when we work with real objects and want to display more than the name
 function createTable(tableArr) {
